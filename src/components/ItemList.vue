6e028337443a964466b5b06709b9dadeeda72dd7
Laki2313 <halych.ira@gmail.com>
2026-04-01 13:12:48 +0300

    Task 1: project scaffold + components skeleton

---
+++ src/components/ItemList.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h2>Список</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ItemList"
+}
+</script>
---
+++ src/components/ItemRow.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <p>Елемент</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ItemRow"
+}
+</script>
