96bc5fa1fe1b12999abce6234b10bdec02ecfe6e
Laki2313 <halych.ira@gmail.com>
2026-04-02 16:56:43 +0300

    Task 3: connect form + list to shared store

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,27 @@
   <div class="app">
     <h1>Task Manager (Pinia)</h1>
 
+    <TaskForm />
     <TaskList />
   </div>
 </template>
 
 <script>
+import TaskForm from './components/TaskForm.vue'
 import TaskList from './components/TaskList.vue'
 
 export default {
   components: {
+    TaskForm,
     TaskList
   }
 }
 </script>
+
+<style>
+.app {
+  max-width: 500px;
+  margin: auto;
+  text-align: center;
+}
+</style>
---
+++ src/components/TaskForm.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="form">
+    <input v-model="title" placeholder="Введіть задачу..." />
+    <button @click="addTask">Додати</button>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+import { useTaskStore } from '../stores/taskStore'
+
+export default {
+  setup() {
+    const title = ref('')
+    const store = useTaskStore()
+
+    const addTask = () => {
+      if (title.value.trim().length < 3) return
+
+      store.addTask(title.value)
+      title.value = ''
+    }
+
+    return { title, addTask }
+  }
+}
+</script>
+
+<style>
+.form {
+  margin-bottom: 20px;
+}
+
+input {
+  padding: 5px;
+  margin-right: 10px;
+}
+</style>
